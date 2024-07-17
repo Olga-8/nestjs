6df20ffb854ed621a694b86149f5b4dcb8ec2b04
Olha Morozova <Olha_Morozova@epam.com>
2024-07-17 13:01:49 +0200

    validation

--- src/user/dto/createUser.dto.ts
+++ src/user/dto/createUser.dto.ts
@@ -1,5 +1,16 @@
+import { IsEmail, IsNotEmpty, Matches } from "class-validator";
+
 export class CreateUserDto {
+    @IsNotEmpty()
+    @Matches(/^[A-Za-z\s]+$/, {
+        message: 'Name can only contain letters and spaces',
+    })
     readonly name: string;
+
+    @IsNotEmpty()
+    @IsEmail()
     readonly email: string;
+
+    @IsNotEmpty()
     readonly password: string;
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,10 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/createUser.dto';
 import { UserEntity } from './user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { sign } from 'jsonwebtoken';   
 import { JwtSekretKey } from '@app/config';
+import { UserResponseInterface } from './types/userResponse.interface';
 
 @Injectable()
 export class UserService {
@@ -13,12 +14,26 @@ export class UserService {
     private userRepository: Repository<UserEntity>) {}
 
     async createUser(createUserDto: CreateUserDto): Promise<UserEntity>{
+
+        const userByEmail = await this.userRepository.findOne({
+            where: { email: createUserDto.email }
+        });
+
+        const userByName = await this.userRepository.findOne({
+            where: { name: createUserDto.name }
+        });
+
+        if (userByEmail || userByName) {
+            throw new HttpException('User already exists', HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+
         const newUser = new UserEntity();
         Object.assign(newUser, createUserDto);
+        
         return await this.userRepository.save(newUser);
     }
 
-    userResponse(user: UserEntity){
+    userResponse(user: UserEntity): UserResponseInterface{
         const token =  this.generateJWT(user);
         const { password, ...result } = user;
         return { 
